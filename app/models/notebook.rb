61f93547957f9e81fb047f0b883851421c8bdc00
Colleen Masterson <cnmasterson@cps.edu>
2014-07-03 15:32:39 -0500

    Paper Trail

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,4 +1,6 @@
 class Notebook < ActiveRecord::Base
-	validates :text, :date, :title, presence: true
+  has_paper_trail
+  
+  validates :text, :date, :title, presence: true
 	validates :title, length: {within: 1..100}
 end
