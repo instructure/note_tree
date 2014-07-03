46f53c3b18574316d4eb4d40dea9528859f64f0b
Abril <abril427@gmail.com>
2014-07-03 16:16:48 -0500

    added user models

--- db/migrate/20140703202029_create_accounts.rb
+++ db/migrate/20140703202029_create_accounts.rb
@@ -2,7 +2,7 @@ class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
       t.references :accountable, :polymorphic => true
-      
+
       t.string :password
       t.string :password_confirmation
       t.string :first_name
