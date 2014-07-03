ddffee9c05db9b56258fc201745e5cc9a76a6420
Abril <abril427@gmail.com>
2014-07-03 11:27:04 -0500

    validation tests

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
-
+gem 'factory_girl'
+gem 'faker'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.2'
--- spec/models/notebook_spec.rb
+++ spec/models/notebook_spec.rb
@@ -1,1 +1,22 @@
-require '../spec_helper'
+# require_relative '../spec_helper'
+require 'spec_helper'
+require 'rails_helper'
+
+
+describe Notebook do
+	# before do
+ #    	@notebook = Notebook.new
+ #  	end
+
+	it "should validate presence of title" do
+		 validate_presence_of :title
+	end
+
+	it "should validate presence of date" do
+		 validate_presence_of :date
+	end 
+
+	it "should validate presence of text" do
+		 validate_presence_of :text
+	end
+end 
