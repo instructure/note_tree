4d2750c386ecb5d4fbd44fa4bda3811c3cdd1997
Abril <abril427@gmail.com>
2014-07-03 15:29:55 -0500

    add accountable

--- db/migrate/20140703201528_create_teachers.rb
+++ db/migrate/20140703201528_create_teachers.rb
@@ -1,6 +1,7 @@
 class CreateTeachers < ActiveRecord::Migration
   def change
     create_table :teachers do |t|
+		t.references :accountable, :polymorphic => true
     end
   end
 end
--- db/migrate/20140703202029_create_accounts.rb
+++ db/migrate/20140703202029_create_accounts.rb
@@ -1,6 +1,8 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
+      t.references :accountable, :polymorphic => true
+      
       t.string :password
       t.string :password_confirmation
       t.string :first_name
