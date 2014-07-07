cc6ccc20d26f0cd6079df8469f6f2cb0119940f9
Colleen Masterson <cnmasterson@cps.edu>
2014-07-07 13:35:49 -0500

    Course Validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,8 @@
 class Course < ActiveRecord::Base
-  validates :short_name, :name, :start_date, :end_date, presence: true
+  validates :short_name, :name, :start_date, :end_date, :description, presence: true
   validates :short_name, length: {within: 1..50}
   validates :name, length: {within: 1..100}
+  validates :description, length: {within:1..500}
 end
 
 
