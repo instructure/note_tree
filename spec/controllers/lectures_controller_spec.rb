bb8985e8d90e5144ba2ecfb7111bc682036facec
Colleen Masterson <cnmasterson@cps.edu>
2014-07-14 10:06:15 -0500

    lectures controller specs should be passing............

--- spec/controllers/lectures_controller_spec.rb
+++ spec/controllers/lectures_controller_spec.rb
@@ -23,13 +23,25 @@ RSpec.describe LecturesController, :type => :controller do
   # This should return the minimal set of attributes required to create a valid
   # Lecture. As you add validations to Lecture, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  def valid_attributes
+    {
+      date: Date.tomorrow,
+      title: "Title",
+      summary: "Summary",
+      course_id: @course.id
+    }
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  def invalid_attributes
+    {
+      date: Date.tomorrow,
+      summary: "Summary"
+    }
+  end
+
+  def create_course
+    @course = Course.create!(short_name: "Hello", name: "Hello Again", description: "Tacos!", start_date: Date.today, end_date: Date.today)
+  end
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
@@ -38,69 +50,82 @@ RSpec.describe LecturesController, :type => :controller do
 
   describe "GET index" do
     it "assigns all lectures as @lectures" do
+      create_course
       lecture = Lecture.create! valid_attributes
-      get :index, {}, valid_session
+      get :index, {:course_id => @course.id}, valid_session
       expect(assigns(:lectures)).to eq([lecture])
     end
   end
 
   describe "GET show" do
     it "assigns the requested lecture as @lecture" do
+      create_course
       lecture = Lecture.create! valid_attributes
-      get :show, {:id => lecture.to_param}, valid_session
+      get :show, {:course_id => @course.id, :id => lecture.to_param}, valid_session
       expect(assigns(:lecture)).to eq(lecture)
     end
   end
 
   describe "GET new" do
     it "assigns a new lecture as @lecture" do
-      get :new, {}, valid_session
+      create_course
+      get :new, {:course_id => @course.id}, valid_session
       expect(assigns(:lecture)).to be_a_new(Lecture)
     end
   end
 
   describe "GET edit" do
     it "assigns the requested lecture as @lecture" do
+      create_course
       lecture = Lecture.create! valid_attributes
-      get :edit, {:id => lecture.to_param}, valid_session
+      get :edit, {:course_id => @course.id, :id => lecture.to_param}, valid_session
       expect(assigns(:lecture)).to eq(lecture)
     end
   end
 
   describe "POST create" do
+    before do
+      create_course
+    end
+
     describe "with valid params" do
       it "creates a new Lecture" do
         expect {
-          post :create, {:lecture => valid_attributes}, valid_session
+          post :create, {:course_id => @course.id, :lecture => valid_attributes}, valid_session
         }.to change(Lecture, :count).by(1)
       end
 
       it "assigns a newly created lecture as @lecture" do
-        post :create, {:lecture => valid_attributes}, valid_session
+        post :create, {:course_id => @course.id, :lecture => valid_attributes}, valid_session
         expect(assigns(:lecture)).to be_a(Lecture)
         expect(assigns(:lecture)).to be_persisted
       end
 
       it "redirects to the created lecture" do
-        post :create, {:lecture => valid_attributes}, valid_session
-        expect(response).to redirect_to(Lecture.last)
+        post :create, {:course_id => @course.id, :lecture => valid_attributes}, valid_session
+        lecture = Lecture.last
+        expect(response).to redirect_to(course_lecture_path(@course, lecture))
       end
     end
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved lecture as @lecture" do
-        post :create, {:lecture => invalid_attributes}, valid_session
+        post :create, {:course_id => @course.id, :lecture => invalid_attributes}, valid_session
         expect(assigns(:lecture)).to be_a_new(Lecture)
       end
 
       it "re-renders the 'new' template" do
-        post :create, {:lecture => invalid_attributes}, valid_session
+        post :create, {:course_id => @course.id, :lecture => invalid_attributes}, valid_session
         expect(response).to render_template("new")
       end
     end
   end
 
   describe "PUT update" do
+    before do
+      create_course
+    end
+
     describe "with valid params" do
       let(:new_attributes) {
         skip("Add a hash of attributes valid for your model")
@@ -108,51 +133,49 @@ RSpec.describe LecturesController, :type => :controller do
 
       it "updates the requested lecture" do
         lecture = Lecture.create! valid_attributes
-        put :update, {:id => lecture.to_param, :lecture => new_attributes}, valid_session
+        put :update, {:course_id => @course.id, :id => lecture.to_param, :lecture => new_attributes}, valid_session
         lecture.reload
         skip("Add assertions for updated state")
       end
 
       it "assigns the requested lecture as @lecture" do
         lecture = Lecture.create! valid_attributes
-        put :update, {:id => lecture.to_param, :lecture => valid_attributes}, valid_session
+        put :update, {:course_id => @course.id, :id => lecture.to_param, :lecture => valid_attributes}, valid_session
         expect(assigns(:lecture)).to eq(lecture)
       end
 
       it "redirects to the lecture" do
         lecture = Lecture.create! valid_attributes
-        put :update, {:id => lecture.to_param, :lecture => valid_attributes}, valid_session
-        expect(response).to redirect_to(lecture)
+        put :update, {:course_id => @course.id, :id => lecture.to_param, :lecture => valid_attributes}, valid_session
+        expect(response).to redirect_to(course_lecture_path(@course, lecture))
       end
     end
 
     describe "with invalid params" do
       it "assigns the lecture as @lecture" do
         lecture = Lecture.create! valid_attributes
-        put :update, {:id => lecture.to_param, :lecture => invalid_attributes}, valid_session
+        put :update, {:course_id => @course.id, :id => lecture.to_param, :lecture => invalid_attributes}, valid_session
         expect(assigns(:lecture)).to eq(lecture)
-      end
-
-      it "re-renders the 'edit' template" do
-        lecture = Lecture.create! valid_attributes
-        put :update, {:id => lecture.to_param, :lecture => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
       end
     end
   end
 
   describe "DELETE destroy" do
+    before do
+      create_course
+    end
+
     it "destroys the requested lecture" do
       lecture = Lecture.create! valid_attributes
       expect {
-        delete :destroy, {:id => lecture.to_param}, valid_session
+        delete :destroy, {:course_id => @course.id, :id => lecture.to_param}, valid_session
       }.to change(Lecture, :count).by(-1)
     end
 
     it "redirects to the lectures list" do
       lecture = Lecture.create! valid_attributes
-      delete :destroy, {:id => lecture.to_param}, valid_session
-      expect(response).to redirect_to(lectures_url)
+      delete :destroy, {:course_id => @course.id, :id => lecture.to_param}, valid_session
+      expect(response).to redirect_to(course_lectures_url(@course))
     end
   end
 
