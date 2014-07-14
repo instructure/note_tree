eeee901a763acbadec198fe4745fc15f0842c7d9
Abril <abril427@gmail.com>
2014-07-14 10:37:55 -0700

    update db migrations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
+# Requires supporting ruby files with custom matchers and macros, etc, i
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
 # in _spec.rb will both be required and run as specs, causing the specs to be
