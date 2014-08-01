58909acc56cacf38b76928b816328289a11f832e
Abril <abril427@gmail.com>
2014-08-01 10:16:50 -0500

    deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to notebook_path(@notebook)
   end
  
+  def destroy
+    @notebook = Notebook.find(params[:notebook_id])
+    @comment = @notebook.comments.find(params[:id])
+    @comment.destroy
+    redirect_to notebook_path(@notebook)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
