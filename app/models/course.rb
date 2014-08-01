bfae8df9b0817ba4f0ea4c0b312936582b52f70b
Abril <abril427@gmail.com>
2014-08-01 11:30:50 -0500

    add valid attributes to controller spec

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
   has_many :lectures
   validates :short_name, :name, :start_date, :end_date, :description, presence: true
-  validates :short_name, length: {within: 1..50}
+  validates :short_name, length: {within: 1..10}
   validates :name, length: {within: 1..100}
   validates :description, length: {within:1..500}
 
