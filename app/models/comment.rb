a7177ff520ae01d59a8a47e48ebfbe4d383facde
Abril <abril427@gmail.com>
2014-08-01 16:04:56 -0500

    account and comment relationships_

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :notebook
+  belongs_to :account
 
   validates :body, :commenter, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140801165050) do
+ActiveRecord::Schema.define(version: 20140801202835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140801165050) do
     t.integer  "notebook_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "account_id"
   end
 
   add_index "comments", ["notebook_id"], name: "index_comments_on_notebook_id", using: :btree
