db2c217f32f3573fcb09e1e7613131f64c5d73b8
Abril <abril427@gmail.com>
2014-08-06 13:33:25 -0500

    notebook version comments spec

--- spec/models/notebook_spec.rb
+++ spec/models/notebook_spec.rb
@@ -3,9 +3,26 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe Notebook do
-	# before do
- #    	@notebook = Notebook.new
- #  	end
+	# login_user
+
+  let(:account){
+    Account.create!(:email => 'user@example.com', :password => 'password', :password_confirmation => 'password', 
+      :first_name => 'Colleen', :last_name => 'Masterson')
+  }
+
+  let(:course){
+    Course.create!(short_name: "Hello", name: "Hello Again", description: "Tacos!", start_date: Date.today, end_date: Date.today)
+  }
+
+  let(:lecture){
+    Lecture.create!(course: course, date: Date.today, title: "lecture title", summary:"Lecture summary")
+  }
+
+  let(:comment){
+  	Comment.create!(body: "This is a comment.", notebook_id: notebook, account_id: account)
+  }
+
+
 	it "should validate presence of title" do
 		notebook = Notebook.new({
 			text: "tacos!"
@@ -18,5 +35,13 @@ describe Notebook do
 			title: "text"
 			})
 		 expect{ notebook.save! }.to raise_error
+	end
+
+	it "should show comments for current notebook version" do
+		notebook = Notebook.new(title: "Notebook", text: "This is a notebook.", lecture: lecture)
+		comment = comment
+		notebook.stub(:text) {"this is new text"}
+		notebook.save
+		expect(notebook.version_comments). to eq([])
 	end
 end 
