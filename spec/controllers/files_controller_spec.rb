d3a6661b31081160b7e81a3b70ed114bc4f7adf1
Colleen Masterson <cnmasterson@cps.edu>
2014-08-07 13:25:48 -0500

    Carrier Wave Scaffolding

---
+++ spec/controllers/files_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe FilesController, :type => :controller do
+  login_teacher
+
+  describe "GET index" do
+    it "returns http success" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET create" do
+    it "returns http success" do
+      get :create
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET destroy" do
+    it "returns http success" do
+      get :destroy
+      expect(response).to be_success
+    end
+  end
+
+end
