fb6982487672829a88ac30c99ac4aaf62b16eda3
Colleen Masterson <cnmasterson@cps.edu>
2014-08-12 16:17:04 -0500

    made email addresses unique

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -3,7 +3,7 @@ module ControllerMacros
   def login_student
     @request.env["devise.mapping"] = Devise.mappings[:account]
     student = Student.create!
-    user = Account.create!(:password => "Password_student", :password_confirmation => "Password_student", :email => "test@email.com", :student => student)
+    user = Account.create!(:password => "Password_student", :password_confirmation => "Password_student", :email => "#{rand(1..100)}test@email.com", :student => student)
     sign_in user
     user
   end
@@ -11,7 +11,7 @@ module ControllerMacros
   def login_teacher
     @request.env["devise.mapping"] = Devise.mappings[:account]
     teacher = Teacher.create!
-    user = Account.create!(:password => "password", :password_confirmation => "password", :email => "teacher@email.com", :teacher => teacher)
+    user = Account.create!(:password => "password", :password_confirmation => "password", :email => "#{rand(1..100)}teacher@email.com", :teacher => teacher)
     sign_in user
     user
   end
