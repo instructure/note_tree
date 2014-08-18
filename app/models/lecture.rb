543fb64981d325598ed9754bebe22ef077f3bd16
Abril <abril427@gmail.com>
2014-08-18 16:07:09 -0500

    lectures index

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -3,8 +3,8 @@ class Lecture < ActiveRecord::Base
   @course_id = '#{id}'
 
   validates :course, :date, :title, :summary, presence: true 
-  validates :title, length: {within:1..100}
-  validates :summary, length: {within:1..500} 
+  validates :title, length: {within:1..50}
+  validates :summary, length: {within:1..250} 
 
   def concat_id 
     "#{self.title}-#{self.date}"
